07f42edd12657ff9686e34c1a3e9ef8b0bf85ab6
florane <qwerty.qa912@gmail.com>
2026-01-13 07:33:45 +0700

    rewritten ehi integration
    changed hooks to affect only loot counters.

--- ehi.lua
+++ ehi.lua
@@ -1,8 +1,15 @@
-if not _G.EHI then
-	return
-
-	Hooks:PostHook(EHI,"IsPlayingCrimeSpree","IsPlayingCrimeSpreeBagged",function()
-		return false
+if _G.EHI then
+	Hooks:PostHook(EHI,"IsLootCounterVisible","IsLootCounterVisibleSpree",function(self)
+		return self:GetTrackerOrWaypointOption("show_loot_counter", "show_waypoints_loot_counter")
+	end)
+	Hooks:PostHook(EHI,"IsSyncedLootCounterVisible","IsSyncedLootCounterVisibleSpree",function(self)
+		return self.IsHost
+	end)
+	Hooks:PostHook(EHI.TrackerUtils,"IsLootCounterVisible","TrackerIsLootCounterVisibleSpree",function(self)
+		if self.super:IsLootCounterVisible() and managers.job:get_memory("EHI_SavedLoot") and not Hooks:GetReturn() then
+			return managers.job:get_memory("EHI_SavedLoot") > 0
+		end
+		return true
 	end)
 end
 
