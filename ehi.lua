b5d0310755b13c38ae7b8242a188b9271c6ef94a
florane <qwerty.qa912@gmail.com>
2026-01-17 08:29:54 +0700

    fixed ehi integration

--- ehi.lua
+++ ehi.lua
@@ -1,15 +1,12 @@
-if _G.EHI then
-	Hooks:PostHook(EHI,"IsLootCounterVisible","IsLootCounterVisibleSpree",function(self)
-		return self:GetTrackerOrWaypointOption("show_loot_counter", "show_waypoints_loot_counter")
-	end)
-	Hooks:PostHook(EHI,"IsSyncedLootCounterVisible","IsSyncedLootCounterVisibleSpree",function(self)
-		return self.IsHost
-	end)
-	Hooks:PostHook(EHI.TrackerUtils,"IsLootCounterVisible","TrackerIsLootCounterVisibleSpree",function(self)
-		if self.super:IsLootCounterVisible() and managers.job:get_memory("EHI_SavedLoot") and not Hooks:GetReturn() then
-			return managers.job:get_memory("EHI_SavedLoot") > 0
-		end
-		return true
-	end)
-end
-
+Hooks:PostHook(EHI,"IsLootCounterVisible","IsLootCounterVisibleSpree",function(self)
+	return self:GetTrackerOrWaypointOption("show_loot_counter", "show_waypoints_loot_counter")
+end)
+Hooks:PostHook(EHI,"IsSyncedLootCounterVisible","IsSyncedLootCounterVisibleSpree",function(self)
+	return self.IsHost
+end)
+Hooks:PostHook(EHI.TrackerUtils,"IsLootCounterVisible","TrackerIsLootCounterVisibleSpree",function(self)
+	if self.super:IsLootCounterVisible() and managers.job:get_memory("EHI_SavedLoot") and not Hooks:GetReturn() then
+		return managers.job:get_memory("EHI_SavedLoot") > 0
+	end
+	return true
+end)
